0a046468bd02792d0ea23a347e111132baae8d42
VIKAS <vikasnishad1873@gmail.com>
2026-04-11 11:58:30 +0530

    force add config

---
+++ config.local.js
@@ -0,0 +1,6 @@
+// Local Supabase config (DO NOT COMMIT)
+// This file is gitignored via .gitignore.
+window.OURPETS_SUPABASE_URL = "https://sbiwiyfashlmmxokhjlp.supabase.co";
+window.OURPETS_SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiaXdpeWZhc2hsbW14b2toamxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ0NTMsImV4cCI6MjA5MTMwMDQ1M30.VB4YV-7O7hPb22ehqe3oYo_3JbWL1kW1mKJH4_mes5Y";
+
